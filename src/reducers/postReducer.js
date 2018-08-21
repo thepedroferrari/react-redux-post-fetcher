7e241f5ddbdb0986b49c2deb6806319f9864c796
Pedro Ferrari <pedro@pedroferrari.com>
2018-08-22 00:32:01 +0200

    Setup the Payload & Dispatch

--- src/reducers/postReducer.js
+++ src/reducers/postReducer.js
@@ -7,6 +7,11 @@ const initalState = {
 
 export default function(state = initialState, action) {
 	switch (action.type) {
+		case FETCH_POSTS:
+			return {
+				...state,
+				items: action.payload
+			};
 		default:
 			return state;
 	}
