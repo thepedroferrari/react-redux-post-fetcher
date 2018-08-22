ce8f4fc0a2c6c035ce22b7f2f1dc70ea0a9852bd
Pedro Ferrari <pedro@pedroferrari.com>
2018-08-22 11:38:57 +0200

    Remove Redux DevTools

--- src/actions/postActions.js
+++ src/actions/postActions.js
@@ -10,7 +10,6 @@ export const fetchPosts = () => (dispatch) => {
 };
 
 export const createPost = (postData) => (dispatch) => {
-	console.log('action called');
 	fetch('https://jsonplaceholder.typicode.com/posts', {
 		method: 'POST',
 		headers: {
